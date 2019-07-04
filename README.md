014590ca5aa5ebeae63b920a013b8b4c965fdd15
Scott Bamford <scott@ambidect.company>
2019-07-04 21:56:47 +0100

    Import initial code.

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # reactstrap-elasticinput
-Reactstrap Input wrapper that provides autosizing to fit the contents when used with type="textarea".
+Reactstrap Input component that autosizes to fit its contents when used with type=\"textarea\".
+
+## Installation
+
+Install react-use-async-callback locally within your project folder, like so:
+
+```shell
+npm install reactstrap-elasticinput
+```
+
+Or with yarn:
+
+```shell
+yarn add reactstrap-elasticinput
+```
+
+## Basic Usage
+
+You can use ElasticInput anywhere you would use an Input, however its elastic autosizing only works if the type of the input is passed
+as "textarea" (the default if no type is passed to EkasticInput)
+
+### Typescript
+```ts
+import { ElasticInput } from 'reactstrap-elasticinput';
+
+<ElasticInput type="textarea" name="..." value="..." />
+```
+
+### Javascript
+```js
+import { ElasticInput } from 'reactstrap-elasticinput';
+
+<ElasticInput type="textarea" name="..." value="..." />
+```
+
+You can pass any of Input's props to ElasticInput and they will be passed through to the wrapped Input.
+
+## CSS and Styling
+
+All ElasticInput components have a CSS class of "elastic" attached to them (via their className prop).
+
+You can use this in CSS to do things such as set a maximum height for example.
+
+```css
+textarea.elastic {
+    max-height: 200px;
+    transition: ease 0.4s;
+}
+```
+
+The elastic size of the componet is always set using the style={{ height: [size]px }} prop and updated each time the user inputs into the component.
+
+## Typescript
+This package is written in typescript and comes with its own bindings.
+
+## License
+
+Licensed under the MIT license.
---
+++ src/ElasticInput.tsx
@@ -0,0 +1,49 @@
+﻿import * as React from 'react';
+import { InputProps, Input } from 'reactstrap';
+
+export interface ElasticInputProps extends InputProps {
+}
+
+/**
+ * Input wrapper that provides autosizing when used with a textarea.
+ * 
+ * @param props
+ */
+export const ElasticInput = (props: ElasticInputProps) => {
+    const { className, onInput, type, ...rest } = props;
+    const [calculatedHeight, setCalculatedHeight] = React.useState<string | number | undefined>(undefined);
+
+    let inputRef = React.useRef<HTMLInputElement>(null);
+
+    // Calcualte the best height for input.
+    const resizeToBestHeight = React.useCallback((target: HTMLInputElement): void => {
+        // Reset the height and then set it based on the scrollHeight.
+        target.style.height = '';
+        let newHeight = target.scrollHeight + 10; /* The 10 provides a nice buffer to avoid flicker */;
+        target.style.height = `${newHeight}px`;
+
+        // Store the height for the next render.
+        setCalculatedHeight(newHeight);
+    }, [setCalculatedHeight]);
+
+    // Calculate height when the user provides input to the control.
+    const onInputWrapper = React.useCallback((event: React.FormEvent<HTMLInputElement>): void => {
+        resizeToBestHeight(event.currentTarget);
+
+        // Pass on the input event.
+        if (onInput) {
+            onInput(event);
+        }
+    }, [resizeToBestHeight, onInput]);
+
+    // Calculate the height when we mount.
+    React.useEffect(() => {
+        if (!calculatedHeight && inputRef.current) {
+            resizeToBestHeight(inputRef.current);
+        }
+    }, [inputRef.current, calculatedHeight, resizeToBestHeight]);
+
+    return (
+        <Input innerRef={inputRef} type={type || 'textarea'} className={`elastic ${className || ''}`} {...rest} onInput={onInputWrapper} style={{ height: calculatedHeight }} />
+    );
+};
